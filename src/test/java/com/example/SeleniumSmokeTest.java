f765deb8c160799a7ff83d5ed4447aced3891f61
akashaadhi001 <akashaadhi1932@gmail.com>
2026-06-21 12:50:35 +0530

    Test and Build

--- src/test/java/com/example/SeleniumSmokeTest.java
+++ src/test/java/com/example/SeleniumSmokeTest.java
@@ -2,18 +2,29 @@ package com.example;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class SeleniumSmokeTest {
 
     @Test
-    public void seleniumClassesAreAvailable() throws Exception {
-        
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
-        
-        driver.quit();
+    public void shouldOpenGooglePage() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--headless=new",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--window-size=1920,1080"
+        );
 
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get("https://www.google.com");
+            Assert.assertTrue(driver.getCurrentUrl().contains("google"));
+            Assert.assertTrue(driver.getTitle().length() > 0);
+        } finally {
+            driver.quit();
+        }
     }
 }
